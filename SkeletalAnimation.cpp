ed015b1955fc034905c4ce038e046862917961f5
Gongzai Li <gli65@uclive.ac.nz>
2022-10-15 20:20:15 +1300

    feat: add a tree

--- SkeletalAnimation.cpp
+++ SkeletalAnimation.cpp
@@ -62,7 +62,7 @@ float view_angle = 0;
 const int TEXTURES_NUM = 2;
 GLuint texID[TEXTURES_NUM]; // init textures number
 string texture_path = "./src/textures/";
-const char* textures[TEXTURES_NUM] = {"floor.tga", "background_3.tga"};
+const char* textures[TEXTURES_NUM] = { "floor.tga", "background_3.tga" };
 
 // upload texture
 void loadTexture()
@@ -85,7 +85,7 @@ void loadTexture()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 
-	
+
 
 	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);//GL_REPLACE  GL_MODULATE
 
@@ -120,85 +120,85 @@ void renderSkeleton(const aiNode* node) {
 // render Chest
 void renderChest() {
 	glPushMatrix();
-		glTranslatef(0, 7, 0);
-		glScalef(14, 20, 4);
-		glutSolidCube(1);
+	glTranslatef(0, 7, 0);
+	glScalef(14, 20, 4);
+	glutSolidCube(1);
 	glPopMatrix();
 }
 
 // render Hip
 void renderHips() {
 	glPushMatrix();
-		glScalef(14, 4, 4);
-		glutSolidCube(1);
+	glScalef(14, 4, 4);
+	glutSolidCube(1);
 	glPopMatrix();
 }
 
 // render Collar
 void renderCollar(int side) {
 	glPushMatrix();
-		glTranslatef(side * 7, 0, 0);
-		glutSolidSphere(2.5, 20, 20);
+	glTranslatef(side * 7, 0, 0);
+	glutSolidSphere(2.5, 20, 20);
 	glPopMatrix();
 }
 
 // render up leg
 void renderUpLeg() {
 	glPushMatrix();
-		glTranslatef(0, -9, 0);
-		glScalef(3, 18, 3);
-		glutSolidCube(1);
+	glTranslatef(0, -9, 0);
+	glScalef(3, 18, 3);
+	glutSolidCube(1);
 	glPopMatrix();
 
 	glPushMatrix();
-		glTranslatef(0, -18, 0);
-		glutSolidSphere(2.5, 20, 20);
+	glTranslatef(0, -18, 0);
+	glutSolidSphere(2.5, 20, 20);
 	glPopMatrix();
 }
 
 // render low leg
 void renderLowLeg() {
 	glPushMatrix();
-		glTranslatef(0, -9, 0);
-		glScalef(3, 18, 3);
-		glutSolidCube(1);
+	glTranslatef(0, -9, 0);
+	glScalef(3, 18, 3);
+	glutSolidCube(1);
 	glPopMatrix();
 }
 
 // reder right foot
 void renderFoot() {
 	glPushMatrix();
-		glTranslatef(0, -1.5, 2);
-		glScalef(3, 3, 7);
-		glutSolidCube(1);
+	glTranslatef(0, -1.5, 2);
+	glScalef(3, 3, 7);
+	glutSolidCube(1);
 	glPopMatrix();
 }
 
 //  reder Up Arm
 void renderUpArm() {
 	glPushMatrix();
-		glTranslatef(0, -6.5, 0);
-		glScalef(2.5, 13, 2.5);
-		glutSolidCube(1);
+	glTranslatef(0, -6.5, 0);
+	glScalef(2.5, 13, 2.5);
+	glutSolidCube(1);
 	glPopMatrix();
 
 	glPushMatrix();
-		glTranslatef(0, -13, 0);
-		glutSolidSphere(2.5, 20, 20);
+	glTranslatef(0, -13, 0);
+	glutSolidSphere(2.5, 20, 20);
 	glPopMatrix();
 }
 
 // render Hand
 void renderHand() {
 	glPushMatrix();
-		glTranslatef(0, 0, 0);
-		glScalef(2.5, 14, 2.5);
-		glutSolidCube(1);
+	glTranslatef(0, 0, 0);
+	glScalef(2.5, 14, 2.5);
+	glutSolidCube(1);
 	glPopMatrix();
 
 	glPushMatrix();
-		glTranslatef(0, -8, 0);
-		glutSolidSphere(3, 20, 20);
+	glTranslatef(0, -8, 0);
+	glutSolidSphere(3, 20, 20);
 
 	glPopMatrix();
 
@@ -207,16 +207,16 @@ void renderHand() {
 
 void renderNeck() {
 	glPushMatrix();
-		glTranslatef(0, 3, 0);
-		glRotatef(90, 1, 0, 0);
-		glutSolidCylinder(1.5, 5, 50, 10);
+	glTranslatef(0, 3, 0);
+	glRotatef(90, 1, 0, 0);
+	glutSolidCylinder(1.5, 5, 50, 10);
 	glPopMatrix();
 }
 
 void renderHead() {
 	glPushMatrix();
-		glTranslatef(0, 2, 0);
-		glutSolidSphere(6, 20, 20);
+	glTranslatef(0, 2, 0);
+	glutSolidSphere(6, 20, 20);
 	glPopMatrix();
 }
 
@@ -232,9 +232,9 @@ void render(const aiNode* node)
 
 	//The scene graph for a skeleton contains at most one mesh per node
 	//Skeleton meshes are always triangle meshes
-	if(node->mNumMeshes > 0)
+	if (node->mNumMeshes > 0)
 	{
-		if (node->mName == aiString("Chest")) 
+		if (node->mName == aiString("Chest"))
 		{
 			renderChest();
 		}
@@ -284,7 +284,7 @@ void render(const aiNode* node)
 		}
 		else if (node->mName == aiString("RightHand") || node->mName == aiString("LeftHand")) {
 			renderHand();
-		}		
+		}
 		else if (node->mName == aiString("Neck")) {
 			renderNeck();
 		}
@@ -356,7 +356,7 @@ void initialise()
 	float ambient[4] = { 0.2, 0.2, 0.2, 1.0 };  //Ambient light
 	float white[4] = { 1, 1, 1, 1 };		    //Light's colour
 	float black[4] = { 0, 0, 0, 1 };
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, white);
@@ -375,7 +375,7 @@ void initialise()
 	if (scene == NULL) exit(1);
 	//printTreeInfo(scene->mRootNode);
 	//printAnimInfo(scene, 0);
-	
+
 	// animation = scene->mAnimations[0];
 
 	tDuration = scene->mAnimations[0]->mDuration;
@@ -395,18 +395,18 @@ void drawFloor()
 	glBindTexture(GL_TEXTURE_2D, texID[0]);
 	glColor3f(1, 1, 1);
 	glBegin(GL_QUADS);
-	for (int i = -5; i < 5; i ++)
+	for (int i = -5; i < 5; i++)
 	{
-		for (int j = -5; j < 5; j ++)
+		for (int j = -5; j < 5; j++)
 		{
 			glNormal3f(0, 1, 0);
-			glTexCoord2f(0.0, 0.0); 
+			glTexCoord2f(0.0, 0.0);
 			glVertex3f(i, 0.01, j);
-			glTexCoord2f(10.0, 0.0); 
+			glTexCoord2f(10.0, 0.0);
 			glVertex3f(i, 0.01, j + 1);
-			glTexCoord2f(10.0, 10.0); 
+			glTexCoord2f(10.0, 10.0);
 			glVertex3f(i + 1, 0.01, j + 1);
-			glTexCoord2f(0.0, 10.0); 
+			glTexCoord2f(0.0, 10.0);
 			glVertex3f(i + 1, 0.01, j);
 		}
 	}
@@ -421,17 +421,41 @@ void drawBackground()
 	glColor3f(1, 1, 1);
 	glBegin(GL_QUADS);
 	glNormal3f(0, 0, -1);
-		glTexCoord2f(0.0, 1.0); 
-		glVertex3f(-5, 10, -5);
-		glTexCoord2f(0.0, 0.0); 
-		glVertex3f(-5, 0, -5);
-		glTexCoord2f(1.0, 0.0); 
-		glVertex3f(5, 0, -5);
-		glTexCoord2f(1.0, 1.0); 
-		glVertex3f(5, 10, -5);
+	glTexCoord2f(0.0, 1.0);
+	glVertex3f(-5, 10, -5);
+	glTexCoord2f(0.0, 0.0);
+	glVertex3f(-5, 0, -5);
+	glTexCoord2f(1.0, 0.0);
+	glVertex3f(5, 0, -5);
+	glTexCoord2f(1.0, 1.0);
+	glVertex3f(5, 10, -5);
 	glEnd();
-	
+
 	glDisable(GL_TEXTURE_2D);
+}
+
+void drawTree()
+{
+	glPushMatrix();
+	glColor4f(0.5, 0., 0., 1.0); //base
+	glTranslatef(0, 2, 0);
+	glScalef(1, 4, 1);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix();               //green 1    
+	glColor4f(0., 1., 0., 1.0);
+	glTranslatef(0, 4, 0);
+	glRotatef(-90, 1, 0, 0);
+	glutSolidCone(3, 2, 20, 20);
+	glPopMatrix();
+
+	glPushMatrix();               //green 2   
+	glColor4f(0., 1., 0., 1.0);
+	glTranslatef(0, 6, 0);
+	glRotatef(-90, 1, 0, 0);
+	glutSolidCone(3, 2, 20, 20);
+	glPopMatrix();
 }
 
 void displayScene()
@@ -442,7 +466,13 @@ void displayScene()
 		drawFloor();
 		if (toggleBackground) drawBackground();
 	glPopMatrix();
-	
+
+	glPushMatrix();
+		glTranslatef(-2, 0, 0);
+		glScalef(0.7, 0.7, 0.7);
+		drawTree();
+	glPopMatrix();
+
 }
 
 void drawBall() {
@@ -453,19 +483,19 @@ void drawBall() {
 void displatBall()
 {
 	glPushMatrix();
-		glColor3f(1, 0, 0);
-		drawBall();
+	glColor3f(1, 0, 0);
+	drawBall();
 	glPopMatrix();
 }
 
 void displayModel()
 {
 	glPushMatrix();
-		glTranslatef(0, -0.1, 0);
-		glScalef(scene_scale, scene_scale, scene_scale);
-		glTranslatef(-scene_center.x, -scene_center.y, -scene_center.z);
-		glColor3f(0, 0.7, 0.7);
-		render(scene->mRootNode);
+	glTranslatef(0, -0.1, 0);
+	glScalef(scene_scale, scene_scale, scene_scale);
+	glTranslatef(-scene_center.x, -scene_center.y, -scene_center.z);
+	glColor3f(0, 0.7, 0.7);
+	render(scene->mRootNode);
 	glPopMatrix();
 }
 
@@ -477,10 +507,10 @@ void displayModelShadow()
 	glDisable(GL_TEXTURE_2D);
 
 	glPushMatrix();
-		glTranslatef(0, 0.05, 0);
-		glMultMatrixf(shadowMat);
-		glScalef(scene_scale, scene_scale, scene_scale);
-		glTranslatef(-scene_center.x, -scene_center.y, -scene_center.z);
+	glTranslatef(0, 0.05, 0);
+	glMultMatrixf(shadowMat);
+	glScalef(scene_scale, scene_scale, scene_scale);
+	glTranslatef(-scene_center.x, -scene_center.y, -scene_center.z);
 	glColor4f(0.1, 0.1, 0.1, 1);
 	render(scene->mRootNode);
 	glPopMatrix();
@@ -502,7 +532,7 @@ void displayBallShadow()
 	glColor4f(0.1, 0.1, 0.1, 1);
 	drawBall();
 	glPopMatrix();
-	
+
 	glEnable(GL_LIGHTING);
 	glEnable(GL_TEXTURE_2D);
 
@@ -526,15 +556,15 @@ void display()
 	glLightfv(GL_LIGHT0, GL_POSITION, lightPosn);
 
 	glPushMatrix();
-		glTranslatef(-scene_center.x, -scene_center.y, -scene_center.z);
-		glRotatef(view_angle, 0, 1, 0);
-		glTranslatef(scene_center.x, scene_center.y, scene_center.z);
+	glTranslatef(-scene_center.x, -scene_center.y, -scene_center.z);
+	glRotatef(view_angle, 0, 1, 0);
+	glTranslatef(scene_center.x, scene_center.y, scene_center.z);
 
-		displayScene();
-		displayModel();
-		displayModelShadow();
-		displatBall();
-		displayBallShadow();
+	displayScene();
+	displayModel();
+	displayModelShadow();
+	displatBall();
+	displayBallShadow();
 	glPopMatrix();
 
 	glutSwapBuffers();
@@ -550,7 +580,7 @@ void updateBall()
 
 		isKicked = false;
 		init_ball = true;
-		
+
 		ball_Vy = ball_Veloctiy * sin(45 * CDR) * cos(45 * CDR);;
 	} {
 		ball_x += ball_Vx / 1000 * timeStep;
@@ -577,11 +607,11 @@ void update(int value) {
 		//cout << tick << endl;
 		updateNodeMatrices(tick);
 		tick += tick_step;
-		
+
 		if ((left_foot_vec.x * scene_scale) >= ball_x + 0.2 && (left_foot_vec.z * scene_scale) >= ball_z - 0.2) {
 			isKicked = true;
 			reset_ball = false;
-			
+
 		}
 
 		if (isKicked) {
@@ -590,14 +620,14 @@ void update(int value) {
 		glutTimerFunc(timeStep, update, tick);
 	}
 
-	
+
 	glutPostRedisplay();
 }
 
 // Camera rotation
 void cameraRotation(int direction) {
 	view_angle += direction * camera_rotation_speed;
-	
+
 	if (view_angle >= 360) {
 		view_angle = 0;
 	}
@@ -608,16 +638,16 @@ void cameraRotation(int direction) {
 
 // Camera zoom
 void cameraZoom(int direction) {
-		//cout << cam_z << endl;
-		cam_z += direction * camera_movement_speed;
+	//cout << cam_z << endl;
+	cam_z += direction * camera_movement_speed;
 
-		if (cam_z <= 2) {
-			cam_z = 2;
-		}
+	if (cam_z <= 2) {
+		cam_z = 2;
+	}
 
-		if (cam_z >= 20) {
-			cam_z = 20;
-		}
+	if (cam_z >= 20) {
+		cam_z = 20;
+	}
 }
 
 // Callback function for special keyboard events
@@ -656,7 +686,7 @@ void keyboard(unsigned char key, int x, int y)
 		reset_ball = true;
 		break;
 	}
-	
+
 	glutPostRedisplay();
 }
 
@@ -678,4 +708,3 @@ int main(int argc, char** argv)
 	glutMainLoop();
 	aiReleaseImport(scene);
 }
-
